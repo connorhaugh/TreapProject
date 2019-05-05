389e12599ca933aae9eccb0319e0fcf941a653a0
Connor Haugh <connorhaugh@vlan401-216.subnet-240.amherst.edu>
2019-05-05 15:30:23 -0400

    RedBlack.java now contains a usable & hypereffcient RB Tree

---
+++ RedBlack.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+public class  RedBlack{
+  public TreeMap<Integer,Integer> RBTree;
+  public RedBlack(){
+    RBTree = new TreeMap<Integer,Integer>;
+  }
+  public void insert(int key){
+    RBTree.put(key,0);
+  }
+  public void delete(int key){
+    RBTree.remove(key);
+  }
+}
