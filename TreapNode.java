e0d11929233036dbf67a15039a3913467cc0e72e
maxliucq <48182206+maxliucq@users.noreply.github.com>
2019-05-06 14:42:58 -0400

    Update TreapNode.java

--- TreapNode.java
+++ TreapNode.java
@@ -1,18 +1,14 @@
 public class TreapNode {
-	
+
+	TreapNode root;
 	int key;
 	int prio;
-	
+
 	TreapNode left;
 	TreapNode right;
-	
+
 	public TreapNode(int key, int prio) {
 		this.key = key;
 		this.prio = prio;
-	}
-	
-	// this method is for test purposes
-	public String print() {
-		return key + ", " + prio;
 	}
 }
